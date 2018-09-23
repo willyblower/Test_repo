92ab90707d4f65313053b66c4faaf33fcfc1cc94
willyblower <guillaume_soufflet@hotmail.com>
2018-09-23 11:04:26 +0200

    typo modification

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test {
 	public static void main(String[] arg){
 
 System.out.println("Hello Wim World!");
-System.out.println("This is a Git test");
+System.out.println("This is a Git Test");
 }
 } 
